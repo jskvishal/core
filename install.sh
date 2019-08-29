6f268021808e52478bb11e9bdc3a8cf861bc217a
ARK Deployer <support@ark.io>
2019-08-29 04:57:28 +0000

    chore: prepare new network config 🎉

--- install.sh
+++ install.sh
@@ -201,7 +201,33 @@ success "Installed system updates!"
 
 heading "Installing ARK Core..."
 
-yarn global add @arkecosystem/core
+shopt -s expand_aliases
+alias ark="$HOME/dgt-bridgechain/packages/core/bin/run"
+echo 'alias digrity="$HOME/dgt-bridgechain/packages/core/bin/run"' >> ~/.bashrc
+
+rm -rf "$HOME/dgt-bridgechain"
+git clone "https://github.com/jskvishal/core" "$HOME/dgt-bridgechain" || FAILED="Y"
+if [ "$FAILED" == "Y" ]; then
+    echo "Failed to fetch core repo with origin 'https://github.com/jskvishal/core'"
+
+    exit 1
+fi
+
+cd "$HOME/dgt-bridgechain"
+HAS_REMOTE=$(git branch -a | fgrep -o "remotes/origin/chore/bridgechain-changes")
+if [ ! -z "$HAS_REMOTE" ]; then
+    git checkout chore/bridgechain-changes
+fi
+
+YARN_SETUP="N"
+while [ "$YARN_SETUP" == "N" ]; do
+  YARN_SETUP="Y"
+  yarn setup || YARN_SETUP="N"
+done
+rm -rf "$HOME/.config/@digrity"
+rm -rf "$HOME/.config/@digrity"
+rm -rf "$HOME/.config/digrity-core"
+
 echo 'export PATH=$(yarn global bin):$PATH' >> ~/.bashrc
 export PATH=$(yarn global bin):$PATH
 ark config:publish
